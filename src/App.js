6f9a4d2c9e7d3bf3e73a1a0c9f4677da973790d6
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-10-17 09:27:58 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './favicon-32x32.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Birthday Project
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
