a8444103e8abd7f0c7fd336818cb0ea7fd9660a0
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-10-20 20:04:13 -0700

    balloon timing and reset logic

--- src/Components/Balloon.js
+++ src/Components/Balloon.js
@@ -1,5 +1,65 @@
-const Balloon = ({ onPop }) => {
-    return <button onClick={() => onPop(1)}>Balloon</button>
+import { gsap } from "gsap";
+import { useEffect, useRef, useState } from "react";
+
+const Balloon = ({ onPop, points, delay, speed, pointsMin = 1 }) => {
+    const [popped, setPopped] = useState(false);
+    const buttonRef = useRef(null);
+    const bobRef = useRef(null);
+    const pointsRef = useRef(points);
+
+    useEffect(() => {
+        gsap.set(buttonRef.current, {yPercent: 100});
+        bobRef.current = gsap.to(buttonRef.current, {
+            yPercent: 0,
+            duration: speed,
+            yoyo: true,
+            repeat: -1,
+            delay: delay,
+            repeatDelay: delay,
+            onRepeat: () => {
+                pointsRef.current = Math.floor(
+                    Math.max(pointsRef.current * .9, pointsMin)
+                  )
+            },
+        })
+        return () => {
+            if (bobRef.current) bobRef.current.kill()
+          }
+    }, [pointsMin, delay, speed]);
+
+    useEffect(() => {
+        if (popped) {
+            pointsRef.current = points;
+
+            bobRef.current.pause();
+
+            gsap.to(buttonRef.current, {
+                yPercent: 100,
+                duration: 0.1,
+                onComplete: () => {
+                    gsap.delayedCall(gsap.utils.random(1, 3), () => {
+                        setPopped(false);
+                        bobRef.current.restart()
+                        .timeScale(bobRef.current.timeScale() * 1.25)
+                    })
+                },
+            })
+        }
+    }, [popped])
+
+    const pop = () => {
+        setPopped(true)
+        onPop(pointsRef.current)
+    }
+
+    return (
+        <button
+            ref={buttonRef}
+            onClick={pop}
+        >
+            Balloon
+        </button>
+    )
 }
 
 export default Balloon;
--- src/Components/PopBalloons.js
+++ src/Components/PopBalloons.js
@@ -3,38 +3,74 @@ import Timer from './Timer';
 import Balloons from './Balloons';
 import Balloon from './Balloon';
 import Score from './Score';
+import { gsap } from "gsap";
+
+const generateBalloons = amount => new Array(amount).fill().map(() => ({
+    speed: gsap.utils.random(0.5, 1),
+    delay: gsap.utils.random(0.5, 4),
+    points: 1
+  }))
 
 const PopBalloons = () => {
     const [playing, setPlaying] = useState(false);
+    const [finished, setFinished] = useState(false);
     const [score, setScore] = useState(0);
+    const [balloons, setBalloons] = useState(generateBalloons(5));
 
     const onPop = points => setScore(score + points);
 
+    const endGame = () => {
+        setPlaying(false)
+        setFinished(true)
+      }
+
+      const startGame = () => {
+        setScore(0)
+        setPlaying(true)
+        setFinished(false)
+      }
+
+
     return (
-    <Fragment>
-        {!playing && <h1>Pop the Balloons</h1>}
-        <button onClick={() => setPlaying(!playing)}>
-            {playing ? "Stop" : "Start"}
-        </button>
-        {playing && (
-            <Fragment>
-                <Score
-                    value={score}
-                />
-                <Timer
-                    time={30000}
-                    onEnd={() => setPlaying(false)}
-                />
-                <Balloons>
-                    <Balloon onPop={onPop}/>
-                    <Balloon onPop={onPop}/>
-                    <Balloon onPop={onPop}/>
-                    <Balloon onPop={onPop}/>
-                    <Balloon onPop={onPop}/>
-                </Balloons>
-            </Fragment>
-        )}
-    </Fragment>
+        <Fragment>
+            {!playing && !finished &&
+                <Fragment>
+                    <h1>Pop the Balloons</h1>
+                    <button onClick={startGame}>Start</button>
+                </Fragment>
+            }
+            {playing && (
+                <Fragment>
+                    <button onClick={endGame}>
+                        End Game
+                    </button>
+                    <Score
+                        value={score}
+                    />
+                    <Timer
+                        time={30000}
+                        onEnd={endGame}
+                    />
+                    <Balloons>
+                    {balloons.map(({delay, speed, points}, index) => (
+                        <Balloon
+                            key={index}
+                            onPop={onPop}
+                            points={points}
+                            delay={delay}
+                            speed={speed}
+                        />
+                        ))}
+                    </Balloons>
+                </Fragment>
+            )}
+            {finished &&
+                <Fragment>
+                    <Score value={score} />
+                    <button onClick={startGame}>Play Again</button>
+                </Fragment>
+            }
+        </Fragment>
     );
 };
 
