9f3e283e2820d8ab87b5d793728ebbd2b2a94612
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-10-21 14:41:41 -0700

    timer adjusted

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -2,22 +2,27 @@ import { useState, useRef, useEffect } from 'react';
 
 
 const Timer = ({ time, interval = 1000, onEnd }) => {
-    const [internalTime, setInternalTime] = useState(time);
-    const timerRef = useRef(time);
-    const timeRef = useRef(time);
-    useEffect(() => {
-        if(internalTime === 0 && onEnd) onEnd()
-    }, [internalTime, onEnd])
-    useEffect(() => {
-        timerRef.current = setInterval(
-            () => setInternalTime(timeRef.current -= interval),
-            interval
-        )
-        return () => {
-            clearInterval(timerRef.current)
-        }
-    }, [internalTime, interval])
-    return <span>{`Time: ${internalTime/1000}`}</span>
+  const [internalTime, setInternalTime] = useState(time);
+  const timerRef = useRef(time);
+  const timeRef = useRef(time);
+  useEffect(() => {
+    if (internalTime === 0 && onEnd) {
+      onEnd()
+    }
+  }, [internalTime, onEnd]);
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setInternalTime((timeRef.current -= interval))
+    }, interval)
+    return () => {
+      clearInterval(timerRef.current)
+    }
+  }, [interval]);
+  return (
+    <span className="info__text">
+      {`${internalTime / 1000}s`}
+    </span>
+  )
 }
 
 export default Timer;
