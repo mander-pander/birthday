abd20fb774da32c39d7071aaccf57338cb795d7b
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-11-01 09:02:14 -0700

    formatting

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,15 +1,16 @@
-import { useState, useRef, useEffect } from 'react';
-
+import { useState, useRef, useEffect } from "react";
 
 const Timer = ({ time, interval = 1000, onEnd }) => {
   const [internalTime, setInternalTime] = useState(time);
   const timerRef = useRef(time);
   const timeRef = useRef(time);
+
   useEffect(() => {
     if (internalTime === 0 && onEnd) {
       onEnd()
     }
   }, [internalTime, onEnd]);
+
   useEffect(() => {
     timerRef.current = setInterval(() => {
       setInternalTime((timeRef.current -= interval))
@@ -18,6 +19,7 @@ const Timer = ({ time, interval = 1000, onEnd }) => {
       clearInterval(timerRef.current)
     }
   }, [interval]);
+
   return (
     <span className="info__text">
       {`${internalTime / 1000}s`}
