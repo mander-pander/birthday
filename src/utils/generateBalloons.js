547acd37492dd0d10a228b94cdf77fefedcc40ce
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-11-02 13:54:18 -0700

    changed harcoded balloon amount to variable amount

--- src/utils/generateBalloons.js
+++ src/utils/generateBalloons.js
@@ -1,7 +1,7 @@
 import {gsap} from 'gsap';
 
-const generateBalloons = () =>
-    new Array(5).fill().map(() => ({
+const generateBalloons = (amount) =>
+    new Array(amount).fill().map(() => ({
         speed: gsap.utils.random(0.5, 1),
         delay: gsap.utils.random(0.5, 4),
         points: 1,
