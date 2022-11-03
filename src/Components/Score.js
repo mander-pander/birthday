680cac6cc605401a0fe3bca62a3c9708f78ca309
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-11-03 11:43:16 -0700

    logic added for score needed to complete game

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -1,4 +1,10 @@
-const Score = ({ value }) => {
+import { useEffect } from "react"
+const Score = ({ value, onEnd }) => {
+    useEffect(() => {
+        if(value === 2) {
+            onEnd()
+        }
+    })
     return <div>{`Score: ${value}`}</div>
 }
 
