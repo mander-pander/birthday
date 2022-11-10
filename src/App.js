f5662e4da914593ab1fb056e64333688909da743
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-11-10 15:40:57 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import PopBalloons from './Components/PopBalloons';
+import Hangman from './Components/Hangman/Hangman';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
-      <PopBalloons />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<PopBalloons />} />
+          <Route path="/Hangman" element={<Hangman />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
