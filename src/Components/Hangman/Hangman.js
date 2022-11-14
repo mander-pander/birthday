5cd7089f56d720b3cfd3ad009bb51a5624e28440
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-11-14 11:06:12 -0800

    hangman trial run

--- src/Components/Hangman/Hangman.js
+++ src/Components/Hangman/Hangman.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const Hangman = () => {
-    return <div>Hangman</div>
+	const word = "TEST";
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G",
+        "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	const [correct, setCorrect] = useState([])
+
+	const hidden = word.split('').map(letter =>
+	correct.includes(letter) ? letter : "_").join(" ");
+
+	return 	<div>
+			<p>{hidden}</p>
+			{alphabet
+			.map((character, index) =>
+			<button
+                key={index} onClick={() => {
+                    if (word.includes(character)) {
+                        setCorrect([...correct, character])
+                    }
+                }}>
+                    {character}
+            </button>)}
+
+			{!hidden.includes("_")}
+
+			</div>
 }
 
 export default Hangman;
