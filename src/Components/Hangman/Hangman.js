8ce9fa90be3f2db2e694ce3adf14c17f837cd68b
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-11-15 08:41:54 -0800

    start game functionality

--- src/Components/Hangman/Hangman.js
+++ src/Components/Hangman/Hangman.js
@@ -6,26 +6,47 @@ const Hangman = () => {
         "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
         "S", "T", "U", "V", "W", "X", "Y", "Z"];
 	const [correct, setCorrect] = useState([])
+    const [playing, setPlaying] = useState(false);
+    const [finished, setFinished] = useState(false);
 
 	const hidden = word.split('').map(letter =>
 	correct.includes(letter) ? letter : "_").join(" ");
 
-	return 	<div>
-			<p>{hidden}</p>
-			{alphabet
-			.map((character, index) =>
-			<button
-                key={index} onClick={() => {
-                    if (word.includes(character)) {
-                        setCorrect([...correct, character])
-                    }
-                }}>
-                    {character}
-            </button>)}
+    const startGame = () => {
+        setPlaying(true)
+        setFinished(false)
+    };
 
-			{!hidden.includes("_")}
+	return 	(
+        <div>
+            {!playing && !finished &&
+                <>
+                    <h1>Hangman</h1>
+                    <button onClick={startGame}>Start</button>
+                </>
+            }
 
-			</div>
+            {playing && (
+                <div>
+                    <p>{hidden}</p>
+
+                    {alphabet.map((character, index) =>
+                        <button
+                            key={index} onClick={() => {
+                                if (word.includes(character)) {
+                                    setCorrect([...correct, character])
+                                }
+                            }}>
+                                {character}
+                        </button>
+                    )}
+
+                    {!hidden.includes("_")}
+                </div>
+            )}
+
+		</div>
+    )
 }
 
 export default Hangman;
