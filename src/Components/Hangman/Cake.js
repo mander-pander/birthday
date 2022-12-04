10f4a5f3651c031a1b4a0a5fd89bcb5bd28dbc78
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-12-04 14:06:14 -0800

    cleaned up code

--- src/Components/Hangman/Cake.js
+++ src/Components/Hangman/Cake.js
@@ -1,10 +1,5 @@
-import {
-    useState
-} from "react";
 const Cake = ({ wrongLetters }) => {
     const errors = wrongLetters.length;
-    console.log(errors);
-    const [show, setShow] = useState(true)
 
     return (
         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
