e8ed8dfd490c6a9af559acd56460c4a0af785c8c
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-02-01 19:22:22 -0800

    lifed up state for child components to allow personalized input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import PopBalloons from './Components/PopBalloons';
+import Welcome from './Components/Welcome';
+import PopBalloons from './Components/Balloons/PopBalloons';
 import Hangman from './Components/Hangman/Hangman';
 import Celebrate from './Components/Celebrate/Celebrate';
 import './App.css';
@@ -7,15 +8,22 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [word, setWord] = useState('');
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<PopBalloons />} />
-          <Route path="/Hangman" element={<Hangman />} />
-          <Route path="/Celebrate" element={<Celebrate />} />
+          <Route exact path="/" element={<Welcome setAge={setAge} setName={setName} setWord={setWord} />} />
+          <Route exact path="/Balloons" element={<PopBalloons age={age} />} />
+          <Route path="/Hangman" element={<Hangman word={word}/>} />
+          <Route path="/Celebrate" element={<Celebrate name={name} />} />
         </Routes>
       </Router>
     </div>
